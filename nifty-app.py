3396d3609eead29ad801dd6b14a104752fd0536b
sushildjoshi <151861259+sushildjoshi@users.noreply.github.com>
2024-01-31 13:20:44 +0000

    done

--- nifty-app.py
+++ nifty-app.py
@@ -11,6 +11,8 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.markdown("""
 This app retrieves the list of the **nifty 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+- On the left hand side, by default all the sectors will be selected when you launch this app first time.
+- Second input feature is to show stock closing price.For this you can use slider to select number of companies & then click on **Show Plots** button.
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
 * **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/NIFTY_500).
 """)
